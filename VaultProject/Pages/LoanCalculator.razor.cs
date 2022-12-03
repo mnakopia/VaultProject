9018ecf1330c7cd4bf94e9cd01d2f03bbb401902
mnakopia <43888047+mnakopia@users.noreply.github.com>
2022-12-03 20:57:56 +0400

    Mc

--- VaultProject/Pages/LoanCalculator.razor.cs
+++ VaultProject/Pages/LoanCalculator.razor.cs
@@ -12,7 +12,7 @@ namespace VaultProject.Pages
         private double _loanAmount { get; set; }
         private int _loanPeriod { get; set; }
         private int _selfEmployedIncome { get; set; }
-        private string _profession { get; set; }
+        private Professions _profession { get; set; }
         private double? _loanTax { get; set; }
         private double _rate { get; set; }
         private double _efectiveRate { get; set; }
@@ -27,6 +27,7 @@ namespace VaultProject.Pages
 
             _loanAmount = 30000;
             _loanPeriod = 36;
+            _rate = 11.54;
             GetLoanTax();
             GetMaxLoanAmount();
         }
@@ -36,6 +37,7 @@ namespace VaultProject.Pages
         public void GetLoanAmount(int loanAmount)
         {
             _loanAmount = loanAmount;
+            GetRate();
             GetLoanTax();
         }
 
@@ -48,12 +50,14 @@ namespace VaultProject.Pages
         public void GetLoanTerm(int loanPeriod)
         {
             _loanPeriod = loanPeriod;
+            GetRate();
             GetLoanTax();
         }
-        public void GetProfessions(string profession)
+        public void GetProfessions(Professions profession)
         {
             _profession = profession;
-            GetLoanTax();
+            GetIndex();
+            GetMaxLoanAmount();
         }
 
         public static double PMT(double yearlyInterestRate, int totalNumberOfMonths, double loanAmount)
@@ -66,9 +70,8 @@ namespace VaultProject.Pages
         {
             if (_loanAmount > 0 && _loanPeriod > 0 && !_isSelfEmployed)
             {
-                _rate = 12.2;
                 _efectiveRate = 18.12;
-                _loanTax = Math.Round(PMT(_rate/100, _loanPeriod, _loanAmount), 2);
+                _loanTax = Math.Round(PMT(_rate / 100, _loanPeriod, _loanAmount), 2);
                 StateHasChanged();
 
             }
@@ -80,24 +83,62 @@ namespace VaultProject.Pages
         {
 
 
-            if (_selfEmployedIncome > 0 && _profession is not null && _isSelfEmployed)
+            if (_selfEmployedIncome > 0 && _isSelfEmployed)
             {
                 _rate = 13.45;
                 _efectiveRate = 15.85;
-                _index = 3.25;
                 _loanMaxAmount = (int)(_selfEmployedIncome * _index);
                 StateHasChanged();
 
             }
 
-           
+
         }
         private void OpenLoanForm()
         {
             DialogOptions closeOnEscapeKey = new DialogOptions() { CloseOnEscapeKey = true };
 
-            DialogService.Show<MakeLoanForm>("",closeOnEscapeKey);
+            DialogService.Show<MakeLoanForm>("", closeOnEscapeKey);
         }
 
+
+        public void GetIndex()
+        {
+
+            _index = _profession switch
+            {
+                Professions.Driver => 5.2,
+                Professions.Nanny => 2.36,
+                _ => 3.25
+            };
+
+
+        }
+
+        public void GetRate()
+        {
+            if (_loanAmount < 2000)
+            {
+                _rate = 35.2;
+
+            }
+            else if (_loanAmount < 4000)
+            {
+                _rate = 25.6;
+            }
+            else if (_loanAmount < 10000)
+            {
+                _rate = 15.67;
+
+            }
+            else
+            {
+                _rate = 11.54;
+            }
+
+
+            _efectiveRate = _rate + 3.27;
+
+        }
     }
 }
